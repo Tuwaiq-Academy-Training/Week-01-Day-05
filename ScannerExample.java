1f54a2f0434c3fc05ff5887ad10ea2f0e5d52474
Saleh-SAFCSP <93971372+Saleh-SAFCSP@users.noreply.github.com>
2022-08-04 14:44:15 +0300

    Add files via upload

---
+++ ScannerExample.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Scanner in=new Scanner(System.in);
+
+//        System.out.println("Please enter your first number : ");
+//        int number1= in.nextInt();
+//
+//        System.out.println("Please enter your second number : ");
+//        int number2=in.nextInt();
+//
+//        System.out.println("Please enter your operation : ");
+//        String operation=in.next();
+//
+//        if(operation.equals("+")){
+//            System.out.println(number1+number2);
+//        }else if(operation.equals("-")){
+//            System.out.println(number1-number2);
+//        }else if (operation.equals("*")){
+//            System.out.println(number1*number2);
+//        }else if (operation.equals("/")){
+//            System.out.println(number1/number2);
+//        }else {
+//            System.out.println("Wrong operation");
+//        }
+
+
+//        System.out.println("Please enter your price : ");
+//
+//        double price= in.nextDouble();
+//
+//        System.out.println("Your VAT is : ");
+//        System.out.println((price)+price*15/100);
+
+
+
+    }
+}
